bd007ecdfcaf50ee9d9ea309fc0da81a65e75660
akankshakasha-bit <akankshakasha@gmail.com>
2026-04-09 15:04:52 +1000

    initial commit

---
+++ Project 1112/activity13.py
@@ -0,0 +1,24 @@
+def square_it_out(start, end):
+
+    squares = []
+    for num in range(start, end + 1):
+        squares.append(num ** 2)
+
+    odd_squares = []
+    even_squares = []
+
+    for sq in squares:
+        if sq % 2 == 0:
+            even_squares.append(sq)
+        else:
+            odd_squares.append(sq)
+
+    print(f"\nRange: {start} to {end}")
+    print(f"All Square Values:  {squares}")
+    print(f"Even Square Values: {even_squares}")
+    print(f"Odd Square Values:  {odd_squares}")
+
+start = int(input("Enter the starting number: "))
+end   = int(input("Enter the ending number: "))
+
+square_it_out(start, end)
