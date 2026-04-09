f891cdfd86713b6d3b1ea47eb7c51461239502ad
akankshakasha-bit <akankshakasha@gmail.com>
2026-04-09 14:26:05 +1000

    initial commit

---
+++ Project 1112/activity1.py
@@ -0,0 +1,20 @@
+friend1_name = "Aisha"
+friend1_birthday = "January 15"
+
+friend2_name = "Carlos"
+friend2_birthday = "March 22"
+
+friend3_name = "Priya"
+friend3_birthday = "July 8"
+
+friend4_name = "Omar"
+friend4_birthday = "October 31"
+
+friend5_name = "Sophie"
+friend5_birthday = "December 5"
+
+print(friend1_birthday, "->", friend1_name)
+print(friend2_birthday, "->", friend2_name)
+print(friend3_birthday, "->", friend3_name)
+print(friend4_birthday, "->", friend4_name)
+print(friend5_birthday, "->", friend5_name)
